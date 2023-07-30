afc6e8fc5102fdb9f6f519bc548c0a9ce3fb9c5f
Lokesh B <lokesh.b.2020.ad@ritchennai.edu.in>
2023-07-30 15:59:04 +0530

    Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import * as math from 'mathjs'
+import update from 'immutability-helper'
+import './App.css'
+import Button from './Button'
+import Buttons from './Buttons'
+import Display from './Display'
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = { operations: [] }
+  }
+  calculatorOperations = () => {
+    let result = this.state.operations.join('')
+    if (result) {
+      result = math.evaluate(result)
+      result = math.format(result, { precision: 14 })
+      result = String(result)
+      this.setState({
+        operations: [result],
+      })
+    }
+  }
+  handleClick = e => {
+    const value = e.target.getAttribute('data-value')
+    switch(value) {
+      case 'clear':
+        this.setState({
+          operations: []
+        })
+        break
+      case 'equal':
+        this.calculatorOperations()
+        break
+      default:
+        const newOperations = update(this.state.operations, {
+          $push: [value],
+        })
+        this.setState({
+          operations: newOperations,
+        })
+        break
+    }
+  }
+  render() {
+    return (
+      <div className='App'>
+        <Display data={this.state.operations} />
+        <Buttons>
+          <Button onClick={this.handleClick} label="C" value="clear" />
+          <Button onClick={this.handleClick} label="7" value="7" />
+          <Button onClick={this.handleClick} label="4" value="4" />
+          <Button onClick={this.handleClick} label="1" value="1" />
+          <Button onClick={this.handleClick} label="0" value="0" />
+
+          <Button onClick={this.handleClick} label="/" value="/" />
+          <Button onClick={this.handleClick} label="8" value="8" />
+          <Button onClick={this.handleClick} label="5" value="5" />
+          <Button onClick={this.handleClick} label="2" value="2" />
+          <Button onClick={this.handleClick} label="." value="." />
+
+          <Button onClick={this.handleClick} label="x" value="*" />
+          <Button onClick={this.handleClick} label="9" value="9" />
+          <Button onClick={this.handleClick} label="6" value="6" />
+          <Button onClick={this.handleClick} label="3" value="3" />
+          <Button label="" value="null" />
+
+          <Button onClick={this.handleClick} label="-" value="-" />
+          <Button onClick={this.handleClick} label="+" size="2" value="+" />
+          <Button onClick={this.handleClick} label="=" size="2" value="equal" />
+
+        </Buttons>
+        <p> Created by Lokesh B</p>
+      </div>
+    )
+  }
 }
 
 export default App;
